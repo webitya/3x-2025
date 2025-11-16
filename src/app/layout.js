7abe59e7f81f6880ed5d9ec2035fb0489868c6a1
Aditya Kumar <webitya@gmail.com>
2025-11-17 01:51:46 +0530

    Aditya

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
         {children}
 
         {/* Partnership Badge */}
-        <div className="fixed bottom-3 left-3 z-50 flex flex-col items-center bg-black/60 backdrop-blur-sm px-2 py-2 rounded-md">
-          <img
+        <div className="fixed bottom-3 left-3 z-50 flex flex-col items-center bg-yellow-50 backdrop-blur-sm px-2 py-2 rounded-md">
+          <imgv
             src="/appolo.png"
             alt="Partner Logo"
             className="w-6 md:w-8 mb-1"
